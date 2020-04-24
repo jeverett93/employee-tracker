0510a776485f391491678bdc5b1471f702693c62
Josh Everett <josheverett93@gmail.com>
2020-04-24 11:30:48 -0500

    created outline of schema for sql

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS tracker_db;
+
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE departments(
+    id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE roles(
+    id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept_id INTEGER(10) NOT NULL,
+    PRIMARY KEY(id) 
+);
+
+CREATE TABLE employees(
+    id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER(10) NOT NULL,
+    manager_id INTEGER(10),
+    PRIMARY KEY(id)
+);
+
