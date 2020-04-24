59030495176a4ce1bd0f9070403242f99b8022be
Josh Everett <josheverett93@gmail.com>
2020-04-24 11:38:15 -0500

    created seeds sql file

--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,14 @@
-USE
+USE tracker_db;
 
-INSERT INTO
-VALUES
+INSERT INTO departments (dept_name)
+VALUES ("IT");
+
+INSERT INTO roles (title, salary, dept_id)
+VALUES ("associate", 60000.00, 1);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Jimmy", "Mack", 1, 10);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Darlene", "McKay", 2, 10);
+
