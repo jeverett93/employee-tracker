319789d5be87dbcc5b1267131d1b8d48ae297e8c
Josh Everett <josheverett93@gmail.com>
2020-04-28 17:06:46 -0500

    edited seeds file

--- seeds.sql
+++ seeds.sql
@@ -5,7 +5,10 @@ INSERT INTO departments (dept_name)
 VALUES ("IT");
 
 INSERT INTO roles (title, salary, dept_id)
-VALUES ("associate", 60000.00, 1);
+VALUES ("team lead", 60000, 1);
+
+INSERT INTO roles (title, salary, dept_id)
+VALUES ("programmer", 40000, 1);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ("Jimmy", "Mack", 1, null);
